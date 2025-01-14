fe472f47969128181f5816bc708728c58f3b4b57
Firet <damianfederico@protonmail.com>
2025-01-14 12:44:11 -0300

    Add Collapsible cards for each node

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,6 +1,7 @@
 import Button from '@mui/material/Button';
 import { useContext } from 'react';
 import { TreeContext } from "../context/TreeContext";
+import CollapsibleCard from './CollapsibleCard';
 
 export default function ListItem(props) {
   const { item, level } = props;
@@ -10,14 +11,20 @@ export default function ListItem(props) {
   return (
     <article
       className="listitem"
-      style={{
-        marginTop: 30 * level + "px"
-      }}
     >
-      <p>
-        {item.name}
-      </p>
-      {isRemovable && <Button onClick={() => deleteNode(item.name)}>Delete Node</Button>}
+      <CollapsibleCard
+        title={item.name}
+        className="listitem"
+        style={{
+          marginTop: 30 * level + "px"
+        }}
+      >
+        <p>
+          Level is {level}
+        </p>
+        {isRemovable && <Button onClick={() => deleteNode(item.name)}>Delete Node</Button>}
+      </CollapsibleCard>
+
     </article>
   );
 }
